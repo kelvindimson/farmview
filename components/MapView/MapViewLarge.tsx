964fe85c6ceaab6544de2e6a154a31dcb6314a57
Kelvin Dim <hello@kelvindimson.com>
2023-09-04 13:12:57 +1000

    Starting a little cleanup

--- components/MapView/MapViewLarge.tsx
+++ components/MapView/MapViewLarge.tsx
@@ -1,24 +1,10 @@
-import React, { useMemo } from 'react'
+import React from 'react'
 import { GoogleMap, Polygon, useLoadScript, Marker } from "@react-google-maps/api";
 
 interface Props {
     farm: Farm;
 }
 const MapViewLarge = ({farm}: Props) => {
-
-    const { isLoaded, loadError } = useLoadScript({
-        googleMapsApiKey: process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY as string,
-    });
-
-    const center = useMemo(() => {
-        return {
-            lat: farm.latitude,
-            lng: farm.longitude
-        }
-    }, [farm.latitude, farm.longitude]);
-
-
-
   return (
     <div>MapViewLarge</div>
   )
